2327cadebf82560a73861708078d16a251ecb91f
TeckFreak <rahulvashistha96@gmail.com>
2021-10-03 12:52:03 +0530

    Updated Dashboard styling

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
+    <p class="text-xl text-gray-500 font-medium"></p>
     <div class="flex flex-wrap content-evenly">
-      <r-card direction="column" class="mb-5" style="min-width: 20rem;">
+      <card direction="column" class="mb-5" style="min-width: 20rem;">
         <p class="text-4xl text-indigo-600 font-bold mb-2">2580</p>
         <p>Total Users</p>
-      </r-card>
-      <r-card direction="column" class="mb-5" style="min-width: 20rem;">
+      </card>
+      <card direction="column" class="mb-5" style="min-width: 20rem;">
         <p class="text-4xl text-red-500 font-bold mb-2">2580</p>
         <p>Total Trial Users</p>
-      </r-card>
-      <r-card direction="column" class="mb-5" style="min-width: 20rem;">
+      </card>
+      <card direction="column" class="mb-5" style="min-width: 20rem;">
         <p class="text-4xl text-green-600 font-bold mb-2">2580</p>
         <p>Total Paid Users</p>
-      </r-card>
-      <r-card direction="column" class="mb-5" style="min-width: 20rem;">
+      </card>
+      <card direction="column" class="mb-5" style="min-width: 20rem;">
         <p class="text-4xl text-blue-500 font-bold mb-2">2580</p>
         <p>Total Licenses</p>
-      </r-card>
+      </card>
     </div>
   </div>
 </template>
