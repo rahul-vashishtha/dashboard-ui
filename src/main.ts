1e30d0583a3cdf4d80daab220788bd1b79382458
TeckFreak <rahulvashistha96@gmail.com>
2022-05-26 14:02:32 +0530

    Added PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/tailwind-light";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue);
+app.mount("#app");
