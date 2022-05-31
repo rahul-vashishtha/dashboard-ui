24b34a4e402e80363491b5579063e6918d735c04
TeckFreak <rahulvashistha96@gmail.com>
2022-06-01 00:32:25 +0530

    Updated routes as per dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,33 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Login from "@/views/Login.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/login",
+    name: "login",
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/",
+    component: () => import("@/layouts/DashboardLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "dashboard",
+        component: () => import("@/views/Dashboard.vue"),
+      },
+      {
+        path: "contact",
+        name: "contact",
+        component: () => import("@/views/Contact.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
